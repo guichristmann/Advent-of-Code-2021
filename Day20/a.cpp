58daa5ba29ab31932c81f460a74f67bc2bdff170
guichristmann.aic <guilherme.christmann@inventec.com>
2022-01-21 22:36:28 +0800

    Adding day 20. First part is a bit hacky, 2nd part is better, realized that wrapping the border solves the issue.

---
+++ Day20/a.cpp
@@ -0,0 +1,215 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <array>
+#include <assert.h>
+#include <bitset>
+
+typedef std::array<bool, 512> Algorithm;
+
+#define DATA_ON 1
+#define DATA_OFF 0
+#define DATA_UNUSED -1
+
+struct Image
+{
+    Image() {};
+    Image(const std::vector<std::vector<int>>& data) : data(data) {};
+
+    void Pad(const int);
+    void Print() const;
+    std::vector<std::vector<int>> data;
+};
+
+void Image::Print() const
+{
+    for ( uint32_t y = 0; y < data.size(); y++ )
+    {
+        for ( uint32_t x = 0; x < data.at(0).size(); x++ )
+        {
+            if ( data.at(y).at(x) == DATA_ON )
+                std::cout << "#";
+            else if ( data.at(y).at(x) == DATA_OFF )
+                std::cout << ".";
+            else if ( data.at(y).at(x) == DATA_UNUSED )
+                std::cout << "_";
+        }
+        std::cout << std::endl;
+    }
+
+    std::cout << std::endl;
+}
+
+void Image::Pad(const int amount)
+{
+    // Pad top rows and bottom rows.
+    std::vector<int> row(data.at(0).size(), DATA_OFF);
+    for ( int i = 0; i < amount; i++ )
+    {
+        data.insert(data.begin(), row);
+        data.insert(data.end(), row);
+    }
+
+    // Pad columns.
+    for ( uint32_t row = 0; row < data.size(); row++ )
+    {
+        for ( int i = 0; i < amount; i++ )
+        {
+            data.at(row).insert(data.at(row).begin(), DATA_OFF);
+            data.at(row).insert(data.at(row).end(), DATA_OFF);
+        }
+    }
+}
+
+void readInput(std::ifstream& is, Algorithm& outAlgo, Image& outImg)
+{
+    std::string line;
+    
+    // First line contains the complete algorithm.
+    is >> line;
+    uint32_t i = 0;
+    for ( const char c : line )
+    {
+        if ( c == '#' )
+        {
+            outAlgo.at(i) = true;
+        }
+        else if ( c == '.' )
+        {
+            outAlgo.at(i) = false;
+        }
+        else
+        {
+            std::cerr << "Unexpected value encountered. Something is wrong!" << std::endl;
+        }
+
+        i++;
+    }
+    assert(i == 512);
+
+    // Read the image. First line used to determine the square dimensions. 
+    while ( is >> line )
+    { 
+        uint32_t size = line.size();
+        std::vector<int> row;
+        for ( const char c : line )
+        {
+            if ( c == '#' )
+            {
+                row.push_back(1);
+            }
+            else if ( c == '.' )
+            {
+                row.push_back(0);
+            }
+            else
+            {
+                std::cerr << "Unexpected value encountered. Something is wrong!" << std::endl;
+            }
+        }
+        outImg.data.push_back(row);
+    }
+}
+
+bool isDataOn(const int x, const int y, const Image& img, Image& outResultImg)
+{
+    //std::cout << x << ", " << y << std::endl;
+    if ( img.data.at(y).at(x) == DATA_ON )
+    {
+        return true;
+    }
+    else if ( img.data.at(y).at(x) == DATA_UNUSED )
+    {
+        outResultImg.data.at(y).at(x) = DATA_OFF;
+    }
+
+    return false;
+}
+
+void operate(const int x, const int y, const Image& img, Image& outResultImg, const Algorithm& algo)
+{
+    std::bitset<9> binaryIdx = {0};
+    if ( isDataOn(x - 1, y - 1, img, outResultImg ) )
+        binaryIdx.set(8);
+    if ( isDataOn(x, y - 1, img, outResultImg ) )
+        binaryIdx.set(7);
+    if ( isDataOn(x + 1, y - 1, img, outResultImg ) )
+        binaryIdx.set(6);
+
+    if ( isDataOn(x - 1, y, img, outResultImg ) )
+        binaryIdx.set(5);
+    if ( isDataOn(x, y, img, outResultImg ) )
+        binaryIdx.set(4);
+    if ( isDataOn(x + 1, y, img, outResultImg ) )
+        binaryIdx.set(3);
+
+    if ( isDataOn(x - 1, y + 1, img, outResultImg ) )
+        binaryIdx.set(2);
+    if ( isDataOn(x, y + 1, img, outResultImg ) )
+        binaryIdx.set(1);
+    if ( isDataOn(x + 1, y + 1, img, outResultImg ) )
+        binaryIdx.set(0);
+    
+    uint32_t decimalIdx = binaryIdx.to_ulong();
+
+    outResultImg.data.at(y).at(x) = algo.at(decimalIdx);
+}
+
+void enhance(Image& img, const Algorithm& algo)
+{
+    Image resultImg(img.data);
+    
+    for ( uint32_t k_y = 1; k_y < img.data.size() - 1; k_y++ )
+    {
+        for ( uint32_t k_x = 1; k_x < img.data.at(0).size() - 1; k_x++ )
+        {
+            operate(k_x, k_y, img, resultImg, algo);
+        }
+    }
+
+    img = resultImg;
+}
+
+uint32_t countBrightPixels(const Image& img)
+{
+    uint32_t count = 0;
+    for ( uint32_t row = 2; row < img.data.size() - 2; row++ )
+    {
+        for ( uint32_t col = 2; col < img.data.at(0).size() - 2; col++ )
+        {
+            if ( img.data.at(row).at(col) == DATA_ON )
+                count++;
+        }
+    }
+
+    return count;
+}
+
+int main(int argc, char* argv[])
+{
+    std::ifstream is(argv[1]); 
+    if ( !is.is_open() )
+    {
+        std::cerr << "Failed to open file!" << std::endl;
+        return 1;
+    }
+
+    Algorithm algo;
+    Image img;
+
+    readInput(is, algo, img);
+
+    img.Pad(15);
+    img.Print();
+    //std::cout << std::endl;
+    enhance(img, algo);
+    img.Print();
+    enhance(img, algo);
+
+    img.Print();
+
+    std::cout << "Result is " << countBrightPixels(img) << std::endl;
+
+    return 0;
+}
